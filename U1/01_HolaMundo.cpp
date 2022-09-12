f60384da6c7034d92ddcd743c50605c0a048f71f
up210692 <up210692@alumnos.upa.edu.mx>
2022-09-12 10:52:50 -0500

    Programa1_HolaMundo

---
+++ U1/01_HolaMundo.cpp
@@ -0,0 +1,23 @@
+/*
+Date: 12/09/22
+Author: Andrea Sofia Orozco Morán 
+Email: up210419@alumnos.upa.edu.mx 
+Description: 
+Mi primer programa en C++
+*/
+
+//------PREPROCESSOR DIRECTIVES------
+
+//io-input/output y stream-flujo 
+//Input/output library between the screen n'keyboard 
+#include <iostream> 
+
+//--------MAIN FUNCTION--------
+int main (){
+
+    //Prints on screen a text 
+    std::cout  << "Hello World this is my first programm";
+
+    //Return 0 means that the programm ends succesfully 
+    return 0;
+}

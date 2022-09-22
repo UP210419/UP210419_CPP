b8e381f631f7424002855e135b73db89dd031ced
UP210671 <up210671@alumnos.upa.edu.mx>
2022-09-22 10:46:13 -0500

    Subiendo archivos Unidad 2

---
+++ U2/02_Renta.cpp
@@ -0,0 +1,30 @@
+/*
+Unidad 2: Uso de if 
+Author: Andrea Sofia Orozco Morán 
+Fecha: 22/09/22
+Descripción: Muestra el uso de if simple 
+*/
+
+//Librería para manejo de entradas y salida de pantalla 
+#include <iostream>
+
+//Uso del namespace para evitar el std::
+using namespace std;
+
+//Función principal de tipo entero 
+int main(){
+
+    /*
+    Los tramos impositivos para la declaración de la renta en un determinado país son los 
+    siguientes: 
+
+    Renta ----------------- Tipo impositivo
+    Menos de $10,000        5%
+    Entre $10 y $20 mil     15%
+    Entre $20 y $35 mil     20%
+    Entre $35 y $60 mil     30%
+    Más de $60,000          45%
+    */
+
+    return 0; 
+}

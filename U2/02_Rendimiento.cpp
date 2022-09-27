df122ace8ae64ea170c3194d469e22ee3a54ac4b
UP210419 <112641366+UP210419@users.noreply.github.com>
2022-09-27 16:34:40 -0500

    Portada

--- U2/02_Rendimiento.cpp
+++ U2/02_Rendimiento.cpp
@@ -3,6 +3,24 @@
 using namespace std; 
 int main(){
 
+    float puntuacion; 
+    float rendimiento; 
+    float inaceptable = 0.0;
+    float aceptable = 0.4;
+    float meritorio = 0.6;
+
+    cout << "Su puntuacion es: \n";
+    cin >> puntuacion; 
+    if(puntuacion = inaceptable){
+        rendimiento = puntuacion * 2400;
+    }else if(puntuacion = aceptable){
+        rendimiento = puntuacion * 2400;
+    }else if(puntuacion = meritorio){
+        rendimiento = puntuacion * 2400;
+    }
+
+    cout << "Tu nivel de rendimiento es " << puntuacion; 
+    cout << "\nTu nivel de rendimiento es " << rendimiento; 
     cout << endl;
     return 0;
 }

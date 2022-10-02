402aac9394b0184f3d6ea31dcd1a322d2d0ec7ab
UP210419 <112641366+UP210419@users.noreply.github.com>
2022-10-02 16:52:55 -0500

    Subiendo

--- README.md
+++ README.md
@@ -10,3 +10,14 @@
 <div align="center"> 
 <img alt="C++" height="180" src="imagenes/curso-cpp-facialix.jpg">
 </div>
+
+<br>
+
+Here you will find all the folders that take during the course , as well as the exercices for each unit. 
+><a href="https://github.com/UP210419/UP210419_CPP/blob/main/U1/Readme.md">Unidad 1</a>
+<br>
+
+><a href="https://github.com/UP210419/UP210419_CPP/blob/main/U2/Readme.md">Unidad 2</a>
+<br> 
+
+><a href="https://github.com/UP210419/UP210419_CPP/blob/main/U3/Readme.md">Unidad 3</a>
--- U2/04_Pizzas.cpp
+++ U2/04_Pizzas.cpp
@@ -11,21 +11,42 @@ Descripción: Muestra uso del if
 using namespace std;
 int main(){
 
-    char resp;
+    char pizza;
+    char ingredientev;
+    char ingredienten;
 
-    cout << "Eliga su piza v/nv";
-    cin >> resp;
-    
-    if(resp == 'v'){
-        cout << "MENÚ VEGETARIANO\n"; 
-        cout << "pimiento\n";
-        cout << "tofu";
-    }else if(resp == 'nv'){
-        cout << "MENÚ NO VEGETARIANO\n";
-        cout << "Peperoni\n";
-        cout << "Jamón\n";
-        cout << "Salmón";
+    cout << "Pizza vegetariana o no vegetariana \n";
+    cin >> pizza;
+    if((pizza == 'v') || (pizza == 'V')){
+        cout << "MENU VEGETARIANO \n"; 
+        cout << "1. Pimiento \n";
+        cout << "2. Tofu \n";
+
+        cout << "\nEliga sus ingredientes \n";
+        cin >> ingredientev; 
+        if(ingredientev = 1){
+            cout << "Pizza con pimiento";
+        }else if(ingredientev = 2){
+            cout << "Pizza con tofu";
+        }
+
+    }else if((pizza == 'n') || (pizza == 'N')){
+        cout << "MENU NO VEGETARIANO\n";
+        cout << "1. Peperoni\n";
+        cout << "2. Jamon\n";
+        cout << "3. Salmon";
+
+        cout << "\nEliga sus ingredientes\n";
+        cin >> ingredienten; 
+        if(ingredienten = 1){
+            cout << "Pizza con peperoni";
+        }else if(ingredienten = 2){
+             cout << "Pizza con jamon";
+        }else if(ingredienten = 3){
+            cout << "Pizza con salmon";
+        }
     }
+    cout << "\nSu pedido es: \n"; 
     cout << endl;
     return 0;
 }

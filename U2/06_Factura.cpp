4e677c78757b0ca20bb84697ec43d2322a2b5f89
UP210419 <112641366+UP210419@users.noreply.github.com>
2022-10-14 18:59:09 -0500

    Unidad 2

--- U2/06_Factura.cpp
+++ U2/06_Factura.cpp
@@ -10,21 +10,25 @@ Descripción: Muestra el uso de la sentencia Do While
 using namespace std; 
 int main(){
 
-    int cant = 0; 
-    int precio; 
-    int total; 
+    int cant; 
+    float precio; 
+    float total; 
 
     do{  
-        cout << "Cantidad de productos que vas a comprar: ";
+        cout << "Cantidad de productos que desea comprar: ";
         cin >> cant; 
+
         if(cant != 0){
-        cout << "Precio de producto: ";
+        cout << "Precio de producto a comprar: ";
         cin >> precio;
         
-        total += precio*cant;   
+        total = total + precio * cant;   
         }
 
     }while(cant != 0);
-    cout << "factura: " << total;
+    cout << "factura \n"; 
+    cout << "---------------- \n";
+    cout << "Pago total: $" << total;
+    cout << endl;
     return 0;
 }
